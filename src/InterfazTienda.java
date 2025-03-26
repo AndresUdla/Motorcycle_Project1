01c80f35b31af511088b12c79764515683fff6e7
Josue Mullo <josue.mullo@udla.edu.ec>
2025-03-26 16:42:13 -0500

    Creacion de clases necesarias para interfaz, y ademas creacion de atributos de la clase Motocicleta

---
+++ src/InterfazTienda.java
@@ -0,0 +1,2 @@
+public class InterfazTienda {
+}
--- src/Motocicleta.java
+++ src/Motocicleta.java
@@ -1,4 +1,67 @@
-package PACKAGE_NAME;
-
 public class Motocicleta {
+    private int id;
+    private String marca;
+    private int cilindraje;
+    private String color;
+    private float precio;
+
+    public Motocicleta(int id, String marca, int cilindraje, String color, float precio) {
+        this.id = id;
+        this.marca = marca;
+        this.cilindraje = cilindraje;
+        this.color = color;
+        this.precio = precio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getCilindraje() {
+        return cilindraje;
+    }
+
+    public void setCilindraje(int cilindraje) {
+        this.cilindraje = cilindraje;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Motocicleta{" +
+                "id=" + id +
+                ", marca='" + marca + '\'' +
+                ", cilindraje=" + cilindraje +
+                ", color='" + color + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
 }
